c26c5e1975890f12b1f96ad4677d7358a4893697
Srinivas Vanam <srinivasvanam@Srinivass-MacBook-Air.local>
2020-08-19 17:00:21 -0400

    wrote slokas in JSON files - still work left

---
+++ src/data/2_poorva_nyasa.js
@@ -0,0 +1,17 @@
+const slokas = [
+  {
+    id: 1,
+    linetel: "అస్య శ్రీ విష్ణోర్దివ్య సహస్రనామ స్తోత్ర మహామంత్రస్య",
+  },
+  {
+    id: 2,
+    linetel: "శ్రీ వేదవ్యాసో భగవాన్ ఋషిః",
+  },
+  {
+    id: 3,
+    linetel: "అనుష్టుప్ ఛందః",
+  },
+];
+
+/* TO BE COMPLETED */
+/* NEED TO DECIDE WHETHER LINE-BY-LINE or ENTIRE TRANSLATION AT ONCE */
---
+++ src/data/5_uttara_peetika.js
@@ -0,0 +1,9 @@
+const slokas = [
+  {
+    id: 1,
+    line1tel: "",
+    line2tel: "",
+    line1eng: "",
+    line2eng: "",
+  },
+];
