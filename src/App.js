90403f8ce43d97351a76eac885221d85535a8aa1
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-17 07:13:27 -0400

    Cleanup + Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Sloka from './components/display/Sloka/Sloka';
 
 function App() {
   return (
+    <>
     <h1 className='test'>Sri Krishna Sharanam Mama!</h1>
+    <h2>This site is for SVS, BG, VSN</h2>
+    <Sloka />
+    </>
   );
 }
 
---
+++ src/components/display/Sloka/Sloka.js
@@ -0,0 +1,15 @@
+const Sloka = () => {
+    return (
+        <h1 style={styles.text}>Sloka text goes here</h1>
+    );
+};
+
+const styles = {
+    text: {
+        padding: "10px 20px",
+        fontSize: "16px",
+        color: "red"
+      },
+};
+
+export default Sloka;
