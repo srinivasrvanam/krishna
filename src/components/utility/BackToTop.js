a4bf2ba4b9b539e921f639be2338dbbe2f7e4698
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-17 15:42:43 -0400

    BackToTop cleanup

--- src/components/utility/BackToTop.js
+++ src/components/utility/BackToTop.js
@@ -1,20 +1,4 @@
-import { useState, useEffect } from "react";
-
 const BackToTop = () => {
-//   const [isVisible, setIsVisible] = useState(false);
-
-//   useEffect(() => {
-//     const toggleVisibility = () => {
-//       if (window.scrollY > 500) {
-//         setIsVisible(true);
-//       } else {
-//         setIsVisible(false);
-//       }
-//     };
-
-//     window.addEventListener("scroll", toggleVisibility);
-//     return () => window.removeEventListener("scroll", toggleVisibility);
-//   }, []);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
