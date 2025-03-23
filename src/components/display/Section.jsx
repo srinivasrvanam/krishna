0efe1efaf8ac76161840ba20fd64cda71821063e
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-23 12:30:58 -0400

    Sloka sections styling updated

--- src/components/display/Section.jsx
+++ src/components/display/Section.jsx
@@ -16,7 +16,7 @@ const Section = ({type, lines}) => {
 
     if (type === "transl" || type === "notes" || type === "anvaya") {
         return (
-          <div className="shadow-lg rounded-lg p-6 max-w-2xl w-full bg-gray-100 dark:bg-gray-800">
+          <div className="shadow-lg rounded-lg px-10 py-6 bg-gray-100 dark:bg-gray-800">
             <h4 className="text-2xl font-semibold text-center text-gray-800 dark:text-gray-100 mb-4">{heading}</h4>
             <ul className="text-gray-800 dark:text-gray-200 list-disc list-outside ">
                 {lines.map((item, index) => (
