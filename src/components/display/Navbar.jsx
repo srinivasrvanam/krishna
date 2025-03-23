3c8985ddd67162e0d6edc274b4b31758682cc941
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-23 13:27:09 -0400

    Nav buttons - hover effect added

--- src/components/display/Navbar.jsx
+++ src/components/display/Navbar.jsx
@@ -7,19 +7,19 @@ const Navbar = () => {
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         
         {/* Site Name */}
-        <Link to="/" className="text-2xl font-bold text-gray-900 dark:text-white">Krishna</Link>
+        <Link to="/" className="text-2xl font-bold">Krishna</Link>
 
         {/* Center Action Buttons */}
         <div className="hidden md:flex space-x-6">
-          <Link to="/SVS_1" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-yellow-400">SV-सुप्रभातम्</Link>
-          <Link to="/SVS_2" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-yellow-400">SV-स्तोत्रम्</Link>
-          <Link to="/SVS_3" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-yellow-400">SV-प्रपत्ति</Link>
-          <Link to="/SVS_4" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-yellow-400">SV-मङ्गलाशासनम्</Link>
-          <Link to="/BG" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-yellow-400">Bhagavad Gita</Link>
+          <Link to="/SVS_1" className="sv-btn-nav">SV-सुप्रभातम्</Link>
+          <Link to="/SVS_2" className="sv-btn-nav">SV-स्तोत्रम्</Link>
+          <Link to="/SVS_3" className="sv-btn-nav">SV-प्रपत्ति</Link>
+          <Link to="/SVS_4" className="sv-btn-nav">SV-मङ्गलाशासनम्</Link>
+          <Link to="/BG" className="sv-btn-nav">Bhagavad Gita</Link>
         </div>
 
         {/* Dark Mode Toggle */}
-        <div className="px-4 py-2 text-2xl rounded-xl bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 cursor-pointer">
+        <div className="px-4 py-1 text-2xl rounded-xl bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 cursor-pointer" >
           <DarkMode />
         </div>
 
