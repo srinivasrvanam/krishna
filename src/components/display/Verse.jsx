52c2c13471528f51c4bb11521fcf3bae88aa07ac
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-23 12:13:54 -0400

    Sloka sections backgrounds updated

--- src/components/display/Verse.jsx
+++ src/components/display/Verse.jsx
@@ -5,13 +5,17 @@ import Section from "./Section";
 
 const Verse = ({ verse }) => {
   return (
-      <div className="flex flex-col items-center border-t-2 border-gray-300 dark:border-gray-700 py-8 mb-4">
-        <div className="text-2xl">{verse.number}</div>
-        <Section type="sloka" lines={verse.sloka} />
-        <Section type="vicheda" lines={verse.vicheda} />
-        <Section type="anvaya" lines={verse.anvaya} />
-        <Section type="transl" lines={verse.transl} />
-        <Section type="notes" lines={verse.notes} />
+      <div className="border-t-2 border-gray-300 dark:border-gray-700 py-8 mb-4">
+        <div className="text-2xl text-center font-bold pb-3">{verse.number}</div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          <Section type="sloka" lines={verse.sloka} />
+          <Section type="vicheda" lines={verse.vicheda} />
+          <Section type="anvaya" lines={verse.anvaya} />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-4">       
+          <Section type="transl" lines={verse.transl} />
+          <Section type="notes" lines={verse.notes} />
+        </div> 
       </div>
   );
 };
