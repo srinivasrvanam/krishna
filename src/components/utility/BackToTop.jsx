b96b506e9b7cf276f53a0209174608ba1820f7da
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-28 10:27:42 -0400

    Back to top to display only after scrolling 500px down

--- src/components/utility/BackToTop.jsx
+++ src/components/utility/BackToTop.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
+
 const BackToTop = () => {
+  const [showButton, setShowButton] = useState(true);
+
+  // Handle scroll event
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 500) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll); // Attach scroll listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Cleanup listener when component unmounts
+    };
+  }, []); // [] (dependency array) indicates the useEffect runs only once when the component mounts
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <button
+    showButton && <button
       onClick={scrollToTop}
       className="sv-btn fixed bottom-15 right-10 "
     >
