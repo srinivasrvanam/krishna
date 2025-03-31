aa8f5f47bef941b5171f6e2ee14b09cb1cfa3bac
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-03-31 06:42:58 -0400

    BG 3-19 created

--- src/content/bg/bg_3.js
+++ src/content/bg/bg_3.js
@@ -1,6 +1,43 @@
 const verses = [
   {
     chapter : 3,
+    number  : 19,
+    sloka   : [
+      'तस्मादसक्त: सततं कार्यं कर्म समाचर |',
+      'असक्तो ह्याचरन्कर्म परमाप्नोति पूरुष: ||'
+    ],
+    vicheda : [
+      'तस्मात् असक्तः सततं कार्यं कर्म समाचर |',
+      'असक्तः हि आचरन् कर्म परम् आप्नोति पूरुष: ||'
+    ],
+    anvaya  : [
+      'तस्मात्, सततं असक्तः कार्यं कर्म समाचर |',
+      'पूरुष: असक्तः कर्म आचरन् हि परम् आप्नोति |'
+    ],
+    transl  : [
+      "Therefore, always perform your prescribed duty without attachment. For by performing work without attachment, a person attains the supreme goal (liberation)." 
+    ],
+    notes   : [
+      "तस्मात् = Therefore",
+      "सक्तः = (सञ्ज् - 1P - to cling/to stick to/to attach) - क्त प्रत्यय (past) = one who is attached)",
+      "असक्तः = one who is not attached (masculine)",
+      "असक्तः = Without attachment",
+      "सततम् = Always",
+      "कार्यम् = That which is to be done (duty)",
+      "कर्म = Action / work",
+      "कार्यं कर्म = Prescribed duty",
+      "समाचर = Perform diligently - लोट्",
+      "हि = Indeed",
+      "असक्तः = Being unattached",
+      "आचरन् = Performing",
+      "कर्म = Action",
+      "परम् = The supreme",
+      "आप्नोति = Attains",
+      "पूरुषः = A person"
+    ],
+  },
+  {
+    chapter : 3,
     number  : 22,
     sloka   : [
       'न मे पार्थास्ति कर्तव्यं त्रिषु लोकेषु किञ्चन |',
