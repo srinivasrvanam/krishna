8ce085b48fe35d79f0b83ad915af8c8681a10537
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-09-22 05:30:06 -0400

    Updated SVS Home Page

--- src/components/pages/SVSHome.jsx
+++ src/components/pages/SVSHome.jsx
@@ -18,18 +18,28 @@ function SVSHome() {
         {/* Second Column (70%) */}
         <div className="w-full lg:w-7/10 flex items-center justify-center p-2">
           <div className="md:px-10 px-5">
-            Suprabhatham is a sacred text dedicated to Lord Venkateswara. It is traditionally recited in the early morning to awaken the deity and seek blessings.
+            Please click the links below to access Suprabhatham, Stotram, Prapatti and Mangalaashasanam.
+            <br /><br />
+            <div className="flex flex-wrap justify-center gap-2 mt-2">
+              <Link to="/svs/1" className="sv-btn">SV-सुप्रभातम्</Link>
+              <Link to="/svs/2" className="sv-btn">SV-स्तोत्रम्</Link>
+              <Link to="/svs/3" className="sv-btn">SV-प्रपत्ति</Link>
+              <Link to="/svs/4" className="sv-btn">SV-मङ्गलाशासनम्</Link>
+            </div>   
+            <br />  
+            Suprabhatham is a sacred text dedicated to Lord Venkateswara. 
+            <br />
+            It is traditionally recited in the early morning to awaken the deity and seek blessings.
             <br />
             It is generally recited along with Stotram, Prapatti and Mangalaashasanam.
             <br />
-            Please click the links below to access Suprabatham, Stotram, Prapatti and Mangalaashasanam.
+            It is composed by the great poet Prativadi Bhayankaram Annangaracharya (13th to 14th century), is a Sanskrit and Tamil scholar, a Sri Vaishnava acharya and is a disciple of Sri Manavala Mamuni.
             <br /><br />
-            <div className="flex flex-wrap justify-center gap-2 mt-2">
-            <Link to="/svs/1" className="sv-btn">SV-सुप्रभातम्</Link>
-            <Link to="/svs/2" className="sv-btn">SV-स्तोत्रम्</Link>
-            <Link to="/svs/3" className="sv-btn">SV-प्रपत्ति</Link>
-            <Link to="/svs/4" className="sv-btn">SV-मङ्गलाशासनम्</Link>
-          </div>      
+            
+            This site contains all slokas in a list format to just recite/chant. 
+            <br />
+            Also, if you click on a particular sloka, you can read more details of the sloka, pada cheda (breaking down of words), anvaya krama (rearranging words to understand meaning in prose format), translations, word-to-word meanings and sanskrit notes for some words.
+            <br />
           <br />
           <div className="text-3xl">Reference Links:</div>
           <SVSLinks />
