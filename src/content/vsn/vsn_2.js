5397875a74cc2e2ad28965d5a06693fb8e0a6429
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-12-25 13:56:09 -0500

    VSN content setup

---
+++ src/content/vsn/vsn_2.js
@@ -0,0 +1,31 @@
+const verses = [
+  {
+    chapter : 2,
+    number  : 1,
+    sloka   : [
+      "शुक्लाम्बरधरं विष्णुं शशिवर्णं चतुर्भुजम् ।",
+      "प्रसन्नवदनं ध्यायेत् सर्वविघ्नोपशान्तये ॥ १॥",
+    ],
+    sloka_tel : [
+    ],
+    vicheda : [
+      "",
+    ],
+    anvaya  : [
+      "",
+    ],
+    transl  : [
+      "",
+    ],
+    essence : [],
+    essence_tel: [
+      "",
+    ],
+    meanings: [],
+    notes   : [
+      "",
+    ],
+  },
+];
+
+export default verses;
---
+++ src/content/vsn/vsn_3.js
@@ -0,0 +1,31 @@
+const verses = [
+  {
+    chapter : 3,
+    number  : 1,
+    sloka   : [
+      "ॐ विश्वं विष्णुर्वषट्कारो भूतभव्यभवत्प्रभुः ।",
+      "भूतकृद्भूतभृद्भावो भूतात्मा भूतभावनः ॥ १॥",
+    ],
+    sloka_tel : [
+    ],
+    vicheda : [
+      "",
+    ],
+    anvaya  : [
+      "",
+    ],
+    transl  : [
+      "",
+    ],
+    essence : [],
+    essence_tel: [
+      "",
+    ],
+    meanings: [],
+    notes   : [
+      "",
+    ],
+  },
+];
+
+export default verses;
