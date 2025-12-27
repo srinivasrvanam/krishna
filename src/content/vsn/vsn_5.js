74e33196f10870889b301a661ac7cd43421f1cd1
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2025-12-27 07:03:38 -0500

    VSN - nyasa and dhyanam are kept separate

---
+++ src/content/vsn/vsn_5.js
@@ -0,0 +1,21 @@
+const verses = [
+  {
+    chapter : 4,
+    number  : 1,
+    sloka   : [
+      "भीष्म उवाच",
+      "इतीदं कीर्तनीयस्य केशवस्य महात्मनः ।",
+      "नाम्नां सहस्रं दिव्यानामशेषेण प्रकीर्तितम् ॥ १॥",
+    ],
+    sloka_tel : [],
+    vicheda : [],
+    anvaya  : [],
+    transl  : [],
+    essence : [],
+    essence_tel: [],
+    meanings: [],
+    notes   : [],
+  },
+];
+
+export default verses;
