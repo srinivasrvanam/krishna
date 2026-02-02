43a4178684d2cce359f2d9a1bc93bc6a5830f1e4
srinivasrvanam <53051498+srinivasrvanam@users.noreply.github.com>
2026-02-01 21:52:39 -0500

    DA image update

--- src/components/pages/DAHome.jsx
+++ src/components/pages/DAHome.jsx
@@ -11,7 +11,7 @@ function DAHome() {
         {/* First Column (30%) */}
         <div className="w-full lg:w-3/10 flex items-start justify-center">
           <div className="flex flex-row justify-center"> 
-            <img src="https://www.harekrsna.de/yasoda/Damodar_s.jpg" alt="Damodara" />
+            <img src="https://www.harekrsna.de/yasoda/Damodar.jpg" alt="Damodara" />
           </div>
         </div>
 
